af262898e339f751f53343fcd46ec0af09fbc858
Arela-Cristina <instagrandine@gmail.com>
2025-03-19 14:44:52 +0100

    Millestone 1 work done

--- src/pages/Lista-Spessa.jsx
+++ src/pages/Lista-Spessa.jsx
@@ -1,5 +1,41 @@
+import { useState } from 'react';
+import style from './Lista-Spessa.module.css';
+
 export default function ListaDellaSpessa() {
+
+
+    const products = [
+        { name: 'Mela', price: 0.5 },
+        { name: 'Pane', price: 1.2 },
+        { name: 'Latte', price: 1.0 },
+        { name: 'Pasta', price: 0.7 },
+    ];
+
+    const [product, setProduct] = useState(products);
+
     return (
-        <div>La mia Lista</div>
+        <section className={style.customList}>
+
+            <div className={style.titleList}>Lista de la Spessa</div>
+
+            {product.map((p, i) => {
+                console.log('product:', p, 'index:', i);
+
+                return (
+                    <div key={i} className={style.list}>
+
+                        <ul>
+                            <li>{p.name}</li>
+                        </ul>
+
+
+                        <ul>
+                            <li>{p.price} €</li>
+                        </ul>
+
+                    </div>
+                );
+            })}
+        </section>
     )
 }
