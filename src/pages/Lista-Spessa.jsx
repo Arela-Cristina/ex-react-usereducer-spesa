643e6750b45ac633b873de572b8b910b6f241a61
Arela-Cristina <instagrandine@gmail.com>
2025-03-19 15:38:35 +0100

    Millestone 2 done

--- src/pages/Lista-Spessa.jsx
+++ src/pages/Lista-Spessa.jsx
@@ -14,34 +14,75 @@ export default function ListaDellaSpessa() {
 
     const [product, setProduct] = useState(products);
 
-    const [addProduct, AddedProducts] = useState([])
+    const [addedProduct, addProducts] = useState([])
+
+    const handleClickAddProduct = (product) => {
+        console.log('product:', product);
+        const addProduct = [...addedProduct, {
+            name: product.name,
+            price: product.price,
+            quantity: 1
+        }];
+
+        addProducts(addProduct);
+    }
+
 
     return (
-        <section className={style.customList}>
+        <>
+            <section className={style.customList}>
 
-            <div className={style.titleList}>Lista de la Spessa</div>
+                <div className={style.titleList}>Lista de la Spessa</div>
 
-            {product.map((p, i) => {
-                console.log('product:', p, 'index:', i);
+                {product.map((p, i) => {
 
-                return (
-                    <div key={i} className={style.list}>
+                    return (
+                        <div key={i} className={style.list}>
 
-                        <ul>
-                            <li>Prodotto: {p.name}</li>
-                        </ul>
+                            <ul>
+                                <li>Prodotto: {p.name}</li>
+                            </ul>
 
 
-                        <ul>
-                            <li>Prezzo: {p.price} €</li>
-                        </ul>
+                            <ul>
+                                <li>Prezzo: {p.price} €</li>
+                            </ul>
 
-                        <button className={style.addButton}>Aggiungi
-                            <img className={style.cart} src={groseryCart} alt="grosery cart" />
-                        </button>
-                    </div>
-                );
-            })}
-        </section>
+                            <button
+                                className={style.addButton}
+                                onClick={() => { handleClickAddProduct(p) }}
+                            >Aggiungi
+                                <img className={style.cart} src={groseryCart} alt="grosery cart" />
+                            </button>
+                        </div>
+                    );
+                })}
+            </section>
+            <section className={style.customList}>
+                <div className={style.titleList}>Prodotti aggiunti al Carrello</div>
+                {addedProduct.map((ap, i) => {
+                    // console.log('product:', ap, 'index:', i);
+
+                    return (
+                        <div key={i} className={style.list}>
+
+                            <ul>
+                                <li>Prodotto: {ap.name}</li>
+                            </ul>
+
+
+                            <ul>
+                                <li>Prezzo: {ap.price} €</li>
+                            </ul>
+
+                            <ul>
+                                <li>Quantita: {ap.quantity}</li>
+                            </ul>
+                        </div>
+                    );
+                })}
+
+            </section>
+        </>
     )
 }
