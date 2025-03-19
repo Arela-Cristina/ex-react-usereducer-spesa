8dc47b335c0209f3ffbda1da08c9e9a64ab887a4
Arela-Cristina <instagrandine@gmail.com>
2025-03-19 17:58:06 +0100

    Millestone 3

--- src/pages/Lista-Spessa.jsx
+++ src/pages/Lista-Spessa.jsx
@@ -12,26 +12,73 @@ export default function ListaDellaSpessa() {
         { name: 'Pasta', price: 0.7 },
     ];
 
+    // stato lista spessa
     const [product, setProduct] = useState(products);
 
+    //stato carrello della spesa
     const [addedProduct, addProducts] = useState([])
 
-    const handleClickAddProduct = (product) => {
-        console.log('product:', product);
-        const addProduct = [...addedProduct, {
-            name: product.name,
-            price: product.price,
-            quantity: 1
-        }];
 
-        addProducts(addProduct);
+    const updateProductQuantity = (product) => {
+
+        const productIsAdded = addedProduct.map((p) =>
+            p.name === product.name
+                ? { ...p, quantity: p.quantity + 1 }
+                : p
+        )
+
+        addProducts(productIsAdded);
     }
 
+
+    const handleClickAddProduct = (product) => {
+
+        const productIsAlreadyAdded = addedProduct.find(p => p.name === product.name)
+
+        if (productIsAlreadyAdded) {
+            updateProductQuantity(productIsAlreadyAdded);
+
+        } else {
+            const addNewProduct = [...addedProduct, {
+                name: product.name,
+                price: product.price,
+                quantity: 1
+            }];
+
+            addProducts(addNewProduct);
+        }
+    }
+
+    const removeFromCart = (product) => {
+        const productIsAdded = addedProduct.map((p) =>
+            p.name === product.name
+                ? { ...p, quantity: p.quantity - 1 }
+                : p
+        ).filter(p => p.quantity > 0)
+
+        addProducts(productIsAdded);
+    }
+
+
+    const handleClickRemoveProduct = (product) => {
+        console.log('prodotto rimosso:', product);
+
+        const productIsAlreadyAdded = addedProduct.find(p => p.name === product.name)
+
+        if (productIsAlreadyAdded) {
+            removeFromCart(productIsAlreadyAdded);
+        }
+    }
+
+    const total = () => {
+        return addedProduct.reduce((total, product) => {
+            return total + (product.price * product.quantity);
+        }, 0).toFixed(2);
+    };
 
     return (
         <>
             <section className={style.customList}>
-
                 <div className={style.titleList}>Lista de la Spessa</div>
 
                 {product.map((p, i) => {
@@ -43,7 +90,6 @@ export default function ListaDellaSpessa() {
                                 <li>Prodotto: {p.name}</li>
                             </ul>
 
-
                             <ul>
                                 <li>Prezzo: {p.price} €</li>
                             </ul>
@@ -54,14 +100,16 @@ export default function ListaDellaSpessa() {
                             >Aggiungi
                                 <img className={style.cart} src={groseryCart} alt="grosery cart" />
                             </button>
+
                         </div>
                     );
                 })}
             </section>
+
             <section className={style.customList}>
                 <div className={style.titleList}>Prodotti aggiunti al Carrello</div>
                 {addedProduct.map((ap, i) => {
-                    // console.log('product:', ap, 'index:', i);
+                    console.log('product:', ap, 'index:', i);
 
                     return (
                         <div key={i} className={style.list}>
@@ -78,9 +126,19 @@ export default function ListaDellaSpessa() {
                             <ul>
                                 <li>Quantita: {ap.quantity}</li>
                             </ul>
+
+                            <button
+                                className={style.addButton}
+                                onClick={() => { handleClickRemoveProduct(ap) }}
+                            >Rimuovi
+                            </button>
+
                         </div>
                     );
                 })}
+
+                <span>Totale da pagare</span>
+                <span>{total()}</span>
 
             </section>
         </>
